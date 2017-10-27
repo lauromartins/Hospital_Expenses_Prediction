46daffbd0c25bf3c8b94a4814429ff1cc90097b6
Lauro C. M. de Paula <lauro_cassio@hotmail.com>
2017-10-27 13:25:31 -0200

    Add files via upload

---
+++ PrevendoDespesasHospitalares.r
@@ -0,0 +1,86 @@
+# Prevendo Despesas Hospitalares
+
+# ***** Esta é a versão 2.0 deste script, atualizado em 23/05/2017 *****
+# ***** Esse script pode ser executado nas versões 3.3.1, 3.3.2, 3.3.3 e 3.4.0 da linguagem R *****
+# ***** Recomendamos a utilização da versão 3.4.0 da linguagem R *****
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+setwd("~/Dropbox/DSA/BigDataAnalytics-R-Azure/Projetos/Projeto03")
+getwd()
+
+# Para esta análise, vamos usar um conjunto de dados simulando despesas médicas hipotéticas 
+# para um conjunto de pacientes espalhados por 4 regiões do Brasil.
+# Esse dataset possui 1.338 observações e 7 variáveis.
+
+# Etapa 1 - Coletando os dados
+despesas <- read.csv("despesas.csv")
+View(despesas)
+
+# Etapa 2: Explorando e Preparando os Dados
+# Visualizando as variáveis
+str(despesas)
+
+# Medias de Tendência Central da variável gastos
+summary(despesas$gastos)
+
+# Construindo um histograma
+hist(despesas$gastos, main = 'Histograma', xlab = 'Gastos')
+
+# Tabela de contingência das regiões
+table(despesas$regiao)
+
+# Explorando relacionamento entre as variáveis: Matriz de Correlação
+cor(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Nenhuma das correlações na matriz são consideradas fortes, mas existem algumas associações interessantes. 
+# Por exemplo, a idade e o bmi (IMC) parecem ter uma correlação positiva fraca, o que significa que 
+# com o aumento da idade, a massa corporal tende a aumentar. Há também uma correlação positiva 
+# moderada entre a idade e os gastos, além do número de filhos e os gasos. Estas associações implicam 
+# que, à media que idade, massa corporal e número de filhos aumenta, o custo esperado do seguro saúde sobe. 
+
+# Visualizando relacionamento entre as variáveis: Scatterplot
+# Perceba que não existe um claro relacionamento entre as variáveis
+pairs(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Scatterplot Matrix
+# install.packages("psych")
+library(psych)
+# Este gráfico fornece mais informações sobre o relacionamento entre as variáveis
+pairs.panels(despesas[c("idade", "bmi", "filhos", "gastos")])
+
+# Etapa 3: Treinando o Modelo
+modelo <- lm(gastos ~ idade + filhos + bmi + sexo + fumante + regiao,
+                data = despesas)
+
+# Similar ao item anterior
+modelo <- lm(gastos ~ ., data = despesas)
+
+# Visualizando os coeficientes
+modelo
+
+# Prevendo despesas médicas 
+previsao <- predict(modelo)
+class(previsao)
+head(previsao)
+
+
+# Etapa 4: Avaliando a Performance do Modelo
+# Mais detalhes sobre o modelo
+summary(modelo)
+
+
+# Etapa 5: Otimizando a Performance do Modelo
+
+# Adicionando uma variável com o dobro do valor das idades
+despesas$idade2 <- despesas$idade ^ 2
+
+# Adicionando um indicador para BMI >= 30
+despesas$bmi30 <- ifelse(despesas$bmi >= 30, 1, 0)
+
+# Criando o modelo final
+modelo_v2 <- lm(gastos ~ idade + idade2 + filhos + bmi + sexo +
+                   bmi30 * fumante + regiao, data = despesas)
+
+summary(modelo_v2)
+
